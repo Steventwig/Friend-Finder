ea11b08a9560aa2948ce771cc967a0704ef9b6cf
steven <steventwig@gmail.com>
2019-10-03 14:58:40 -0700

    added friends list and set up server.js

---
+++ Friend-finder/app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Bob",
+		photo: "https://images.pexels.com/photos/2118953/pexels-photo-2118953.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Sam",
+		photo: "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Sally",
+		photo: "https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
