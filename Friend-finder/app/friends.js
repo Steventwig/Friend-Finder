b00c056d48bc37c5b1f982adfa0172902c193dd3
steven <steventwig@gmail.com>
2019-10-08 17:03:48 -0700

    text added and deployed to heroku

--- Friend-finder/app/friends.js
+++ Friend-finder/app/friends.js
@@ -1,53 +1,21 @@
-var friends = [
+var users = {
+friends: [
 	
 	{
 		name: "Bob",
 		photo: "https://images.pexels.com/photos/2118953/pexels-photo-2118953.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-		scores: [
-			"5",
-			"1",
-			"4",
-			"3",
-			"1",
-			"5",
-			"1",
-			"4",
-			"3",
-			"1"
-		]
+		scores: ["5","1","4","3","1"]
 	},
 	{
 		name: "Sam",
 		photo: "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-		scores: [
-			"3",
-			"1",
-			"5",
-			"4",
-			"1",
-			"5",
-			"1",
-			"1",
-			"5",
-			"3"
-		]
+		scores: ["4","3","5","1","1"]
 	},
 	{
 		name: "Sally",
 		photo: "https://images.pexels.com/photos/2218786/pexels-photo-2218786.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-		score: [
-			"3",
-			"1",
-			"4",
-			"1",
-			"4",
-			"3",
-			"5",
-			"1",
-			"5",
-			"4"		
-		]
+		score: ["1","1","4","3","2"]
 	}
-];
+]};
 
-module.exports = friends;
+module.exports = users;
